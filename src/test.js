6e24dab70fa7af0ceba44ed49a159e29f31df03c
mark <ahrimansource@gmail.com>
2023-02-22 20:47:15 +0800

    工具类  1.0.4  1 browserEnv.ts 工具类规划

--- src/test.js
+++ src/test.js
@@ -1,44 +1,20 @@
-let qs = require("qs")
-let num = 1.0000000003
-let str = '1name'
-let boo = true
-let arr = [1, 3]
-let obj = {name: 1}
-let fun = function () {
-    return '1'
+function netWork(time) {
+    let data = {
+        status: 200,
+        data: {
+            name: 1
+        }
+    }
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(data)
+        }, 1000 * time)
+    })
+}
+async function foo() {
+ return  await netWork(2)
 }
 
-let oooo = {
-    type: 'boolean',
-    data: JSON.stringify(boo)
-}
-
-console.log(JSON.stringify(oooo));
-let objX = JSON.parse(JSON.stringify(oooo))
-let result = ''
-if (objX.type === 'object' || objX.type === 'number' || objX.type === 'boolean') {
-    result = JSON.parse(objX.data)
-} else if (objX.type === 'string') {
-    result = objX.data
-}
-
-console.log(typeof result, result);
-
-
-// test_qs()
-
-function test_qs() {
-    console.log(oooo);
-    console.log(qs.stringify(oooo));
-    console.log(qs.parse(qs.stringify(oooo)));
-}
-
-console.log('====');
-
-// test_JSON()
-
-function test_JSON() {
-    console.log(oooo);
-    console.log(JSON.stringify(oooo));
-    console.log(JSON.parse(JSON.stringify(oooo)));
-}
+foo().then(res => {
+    console.log(res);
+})
