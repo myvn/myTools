13b4488f1ccf19c34720dec359cc4cf0685959a9
mark <ahrimansource@gmail.com>
2023-02-22 20:34:52 +0800

    工具类  1.0.4  1 browserEnv.ts 工具类规划

--- src/utils/browserEvn/browserEnv.ts
+++ src/utils/browserEvn/browserEnv.ts
@@ -33,6 +33,11 @@ let browserEnv = {
         return ''
     },
     isAliMin: () => {
+        let arr=[1,2,3,4,5]
+        arr.forEach((item, index) => {
+            console.log(item);
+            console.log(index);
+        })
         return ''
     }
 }
