13d531fb1235b6af0e4d612c7fa49227bd739a45
mark <ahrimansource@gmail.com>
2023-02-22 22:08:42 +0800

    工具类  1.0.4  1 test.js promise

--- src/test.js
+++ src/test.js
@@ -1,20 +1,21 @@
 function netWork(time) {
-    let data = {
-        status: 200,
-        data: {
-            name: 1
-        }
-    }
+    let data = {time: time}
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(data)
         }, 1000 * time)
     })
 }
-async function foo() {
- return  await netWork(2)
+
+async function foo(time) {
+    return await netWork(time)
 }
 
-foo().then(res => {
-    console.log(res);
-})
+foo(2)
+    .then(res => {
+        console.log(res);
+        return foo(3)
+    })
+    .then(res2 => {
+        console.log(res2);
+    })
