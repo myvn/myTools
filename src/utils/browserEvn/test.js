6a6bb125c2e3d32bf8cbbcb9c2189136683f0eb4
mark <ahrimansource@gmail.com>
2023-02-27 19:39:17 +0800

    工具类  1.0.4 节流

--- src/utils/browserEvn/test.js
+++ src/utils/browserEvn/test.js
@@ -264,3 +264,21 @@ export const localStorageSetExpire = (key, value, expire) => {
 export const strReplace = (str, valueOne, valueTwo) => {
     return str.replace(new RegExp(valueOne,'g'), valueTwo)
 }
+
+/**
+ * 节流
+ * @param {*} func 执行函数
+ * @param {*} delay 节流时间,毫秒
+ */
+export const throttle = function(func, delay) {
+    let timer = null
+    return function() {
+        if (!timer) {
+            timer = setTimeout(() => {
+                func.apply(this, arguments)
+                // 或者直接 func()
+                timer = null
+            }, delay)
+        }
+    }
+}
