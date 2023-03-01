6a0324791ced69dafbcd4abe2b5622de77fb5def
mark <ahrimansource@gmail.com>
2023-03-01 20:13:37 +0800

    工具类  1.0.4 test checkBrowser2

--- src/test.ts
+++ src/test.ts
@@ -223,6 +223,16 @@ export const checkBrowser = () => {
     }
     return Object.keys(obj)[Object.values(obj).indexOf(true)]
 };
+export const checkBrowser2 = () => {
+    const u = navigator.userAgent;
+    const obj = {
+        trident: u.indexOf("Trident") > -1, //IE内核
+        presto: u.indexOf("Presto") > -1, //opera内核
+        webKit: u.indexOf("AppleWebKit") > -1, //苹果、谷歌内核
+        gecko: u.indexOf("Gecko") > -1 && u.indexOf("KHTML") == -1, //火狐内核
+    }
+    return Object.keys(obj)[Object.values(obj).indexOf(true)]
+};
 /**
  * 判断是终端类型,值有ios,android,iPad
  */
