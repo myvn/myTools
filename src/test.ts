533ad8acd9a1d06ebd8000bd54f53f803714f0d5
mark <ahrimansource@gmail.com>
2023-03-07 16:45:15 +0800

    工具类  1.0.4 arrTwoToArrObjXY

--- src/test.ts
+++ src/test.ts
@@ -589,6 +589,15 @@ export const arrTwoToArrObjX = (arrOne, arrTwo, oneKey, twoKey) => {
         return arrOne.map((oneKey, i) => ({ oneKey, twoKey: arrTwo[i] }))
     }
 }
+export const arrTwoToArrObjXY = (arrOne, arrTwo, oneKey, twoKey) => {
+    if(!oneKey&&!twoKey){
+        return arrOne.map((oneKey, i) => ({ [oneKey]:arrTwo[i] }))
+        // 或者,此方法针对将 arrTwo 的索引作为 key 的情况,arrTwo 值会覆盖 arrOne
+        // return Object.assign({}, arrOne, arrTwo)
+    }else{
+        return arrOne.map((oneKey, i) => ({ oneKey, twoKey: arrTwo[i] }))
+    }
+}
 export const arrTwoToArrObjXff = (arrOne, arrTwo, oneKey, twoKey) => {
     if(!oneKey&&!twoKey){
         return arrOne.map((oneKey, i) => ({ [oneKey]:arrTwo[i] }))
