35d57e4393bdb57bbbb4cb9d580ff273b9da785c
mark <ahrimansource@gmail.com>
2023-03-11 20:36:59 +0800

    工具类  1.0.6 1 money 2 arrAndSet 3 arrIntersection 4 arrTwoToArrObj 5 arrObjSum

--- src/utils/test/test.ts
+++ src/utils/test/test.ts
@@ -71,3 +71,12 @@ export const arrTwoToArrObj = (arrOne, arrTwo, oneKey, twoKey) => {
         return arrOne.map((oneKey, i) => ({ oneKey, twoKey: arrTwo[i] }))
     }
 }
+
+/**
+ * 数组对象求和
+ * @param {Object} arrObj 数组对象
+ * @param {String} key 数组对应的 key 值
+ */
+export const arrObjSum = (obj) => {
+    return obj.reduce((prev, cur) => prev + cur.key, 0)
+}
