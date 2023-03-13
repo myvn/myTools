6bf8c59189c853896c4f5b739a8194a3755fcb26
mark <ahrimansource@gmail.com>
2023-03-13 18:15:34 +0800

    工具类 test 1.0.6 1  sessionStorageSetExpire5

--- src/utils/test/test.ts
+++ src/utils/test/test.ts
@@ -268,3 +268,10 @@ export const sessionStorageSetExpire4 = (key, value, expire) => {
         sessionStorage.removeItem(key)
     }, expire)
 }
+export const sessionStorageSetExpire5 = (key, value, expire) => {
+    if (typeof (value) === 'object') value = JSON.stringify(value)
+    sessionStorage.setItem(key, value)
+    setTimeout(() => {
+        sessionStorage.removeItem(key)
+    }, expire)
+}
