fd7caefeb5ea49018867fdd9b816184226a573ee
mark <ahrimansource@gmail.com>
2023-03-15 11:00:44 +0800

    工具类 test 1.0.6 arrAndSet6

--- src/utils/test/test.ts
+++ src/utils/test/test.ts
@@ -60,6 +60,10 @@ export const arrAndSet5 = (arrOne, arrTwo) => {
     return arrOne.concat(arrTwo.filter(v => !arrOne.includes(v)))
 }
 
+export const arrAndSet6 = (arrOne, arrTwo) => {
+    return arrOne.concat(arrTwo.filter(v => !arrOne.includes(v)))
+}
+
 /**
  * 数组交集,只支持一维数组
  * @param {Array} arrOne
