4df45626417a80fade9155934366fea2307d1c48
mark <ahrimansource@gmail.com>
2023-03-25 09:07:20 +0800

    arrIntersection147

--- src/utils/test/test2.ts
+++ src/utils/test/test2.ts
@@ -107,3 +107,7 @@ export const arrIntersection117 = (arrOne, arrTwo) => {
 export const arrIntersection147 = (arrOne, arrTwo) => {
     return arrOne.filter(v => arrTwo.includes(v))
 }
+
+export const arrIntersection148 = (arrOne, arrTwo) => {
+    return arrOne.filter(v => arrTwo.includes(v))
+}
