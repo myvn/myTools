ecda1451a2397c966263cca4cef7553ea941cf87
mark <ahrimansource@gmail.com>
2023-04-04 22:47:56 +0800

    arrTwoToArrObjId134

--- src/utils/test/test2.ts
+++ src/utils/test/test2.ts
@@ -476,3 +476,12 @@ export const arrTwoToArrObjId133 = (arrOne, arrTwo, oneKey, twoKey) => {
         return arrOne.map((oneKey, i) => ({oneKey, twoKey: arrTwo[i]}))
     }
 }
+export const arrTwoToArrObjId134 = (arrOne, arrTwo, oneKey, twoKey) => {
+    if (!oneKey && !twoKey) {
+        return arrOne.map((oneKey, i) => ({[oneKey]: arrTwo[i]}))
+        // 或者,此方法针对将 arrTwo 的索引作为 key 的情况,arrTwo 值会覆盖 arrOne
+        // return Object.assign({}, arrOne, arrTwo)
+    } else {
+        return arrOne.map((oneKey, i) => ({oneKey, twoKey: arrTwo[i]}))
+    }
+}
