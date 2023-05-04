20a8474ae11df849956377cb513a7e4e0caf479a
mark <ahrimansource@gmail.com>
2023-05-04 13:42:39 +0800

    arrConcat2_1

--- src/utils/test/test1.ts
+++ src/utils/test/test1.ts
@@ -52,3 +52,7 @@ export const arrConcat1_9 = (arrOne, arrTwo) => {
 export const arrConcat1_10 = (arrOne, arrTwo) => {
     return arrOne.concat(arrTwo.filter(v => !arrOne.includes(v)))
 }
+
+export const arrConcat2_1 = (arrOne, arrTwo) => {
+    return arrOne.concat(arrTwo.filter(v => !arrOne.includes(v)))
+}
