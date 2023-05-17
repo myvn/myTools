cc7d7599e7457a5a20698ea349bd2a0b5b8faf43
mark <ahrimansource@gmail.com>
2023-05-17 17:47:43 +0800

    sessionStorageSetExpire_1_8

--- src/utils/test/test.ts
+++ src/utils/test/test.ts
@@ -469,3 +469,12 @@ export const sessionStorageSetExpire_1_7 = (key, value, expire) => {
         sessionStorage.removeItem(key)
     }, expire)
 }
+
+
+export const sessionStorageSetExpire_1_8 = (key, value, expire) => {
+    if (typeof (value) === 'object') value = JSON.stringify(value)
+    sessionStorage.setItem(key, value)
+    setTimeout(() => {
+        sessionStorage.removeItem(key)
+    }, expire)
+}
