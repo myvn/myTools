c96f2b74ee60b2372d646bdef66e653b7df3f871
mark <ahrimansource@gmail.com>
2023-05-31 15:52:16 +0800

    sessionStorageSetExpire_2_3

---
+++ src/utils/test/log.ts
@@ -0,0 +1,10 @@
+export default // @ts-ignore
+class Log extends console{
+    constructor() {
+        super();
+    }
+    log(text){
+        console.log('',text)
+        console.log('',text)
+    }
+}
