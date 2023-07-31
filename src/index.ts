630301cf2b4e9df734473c70f2979c238a360dc3
mark <ahrimansource@gmail.com>
2023-07-31 13:55:06 +0800

    randomAny lib

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,8 @@
 // 随机字符
-import {randomAny} from "./utils/randomAny";
-
-// 数据存储工具类
-import StorageData from "./utils/storageData";
-import JsSm4Client from "./utils/encrypt/JsSm4Client";
-// 全局置灰
-import {setGrayscale} from "./utils/style/setGrayscale";
+import { randomAny } from "./utils/randomAny";
 
 export {
-    // 全局置灰
-    setGrayscale,
-    // 数据存储工具类
-    StorageData,
-    // 随机字符串生成
-    randomAny,
-    // 国密4 加密
-    JsSm4Client
-}
+  // 随机字符串生成
+  randomAny
+};
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,92 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
-
+const isProduction = process.env.NODE_ENV === "production";
 const path = require("path");
+
+// 打包输出 index.html
 // const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// workService
 // const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+
+// 清理dist
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const isProduction = process.env.NODE_ENV === "production";
+// 使用NodePolyfillPlugin插件，以便在打包过程中自动添加所需的Node.js全局变量和方法的polyfill。
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
-const stylesHandler = MiniCssExtractPlugin.loader;
+const nodeExternals = require('webpack-node-externals')
 
 const config = {
-  entry: path.join(__dirname, "/src/index.ts"),
-  output: {
-    path: path.join(__dirname, "dist"),
-    library: "tools", // string,
-    // 导出库(exported library)的类型
-    libraryTarget: "umd",
-    globalObject: "this",
-  },
-  devServer: {
-    open: true,
-    host: "localhost",
-  },
-  plugins: [
-   /* new HtmlWebpackPlugin({
-      template: "index.html",
-    }),*/
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/i,
-        loader: "ts-loader",
-        exclude: ["/node_modules/"],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [stylesHandler, "css-loader", "postcss-loader", "sass-loader"],
-      },
-      {
-        test: /\.css$/i,
-        use: [stylesHandler, "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
-      },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
+    entry: path.join(__dirname, "/src/index.ts"),
+    output: {
+        path: path.join(__dirname, "dist"),
+        library: "kits", // string,
+        // 导出库(exported library)的类型
+        // 这里详情看 https://zhuanlan.zhihu.com/p/108216236
+        libraryTarget: "umd", //
+        umdNamedDefine: true,
+        globalObject: "this",
+    },
+    devServer: {
+        open: true,
+        host: "localhost",
+    },
+    plugins: [
+       //  html 测试
+       /* new HtmlWebpackPlugin({
+            template: "index.html",
+        }),*/
+        new CleanWebpackPlugin(),
+        new NodePolyfillPlugin(),
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
-  },
+    performance: {
+        // hints: false, // 关闭新能优化提示
+        hints: "warning", // 枚举
+        maxAssetSize: 300000, // 整数类型（以字节为单位）
+        maxEntrypointSize: 500000, // 整数类型（以字节为单位）
+        assetFilter: function (assetFilename) {
+            // 提供资源文件名的断言函数
+            // 只给出js与css文件的性能提示
+            return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/i,
+                use: [
+                    {
+                        loader: "ts-loader",
+                        options: {
+                            transpileOnly: false // 只做语言转换，而不做类型检查
+                        }
+                    }
+                ],
+                exclude: ["/node_modules/"],
+
+            },
+
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
+        fallback: {
+            http: require.resolve('stream-http')
+        }
+    },
 };
 
 module.exports = () => {
-  if (isProduction) {
-    config.mode = "production";
-
-    // config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-  } else {
-    config.mode = "development";
-  }
-  return config;
+    if (isProduction) {
+        config.mode = "production";
+        // config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+    } else {
+        config.mode = "development";
+    }
+    config.externals = [nodeExternals()]  // 忽略Node.js核心模块
+    return config;
 };
