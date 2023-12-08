40bd2e89e8ecf9d10dd6b89a89959a76af665b9a
mark <ahrimansource@gmail.com>
2023-12-08 16:48:44 +0800

    vue3 原理 1 处理数据

--- src/study/vue3.js
+++ src/study/vue3.js
@@ -1,19 +1,20 @@
 let per = {
   name: "jack",
-  age: 18
-}
+  age: 18,
+  sex: "男"
+};
 let p = new Proxy(per, {
   get(target, propsName) {
     // return target[propsName]
-    return Reflect.get(target, propsName)
+    return Reflect.get(target, propsName);
   },
   set(target, propsName, value) {
     // target[propsName] = value
-    return Reflect.set(target, propsName, value)
+    return Reflect.set(target, propsName, value);
   },
   deleteProperty(target, propsName) {
     // delete target[propsName]
-    return Reflect.deleteProperty(target, propsName)
+    return Reflect.deleteProperty(target, propsName);
 
   }
-})
+});
