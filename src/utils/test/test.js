dc0e34df7571ada72eefe5923458ffdde63d1e87
mark <ahrimansource@gmail.com>
2023-12-14 15:56:12 +0800

    setGrayscale

--- src/utils/test/test.js
+++ src/utils/test/test.js
@@ -17,3 +17,11 @@ function setGrayscale(num = 100, domId) {
     console.log(`没有id为${domId}的dom对象`);
   }
 }
+function setGrayscale1(num = 100, domId) {
+  let html_box = document.getElementById(`${domId ?? "app"}`);
+  if (html_box) {
+    html_box.style.filter = `grayscale(${num}%)`;
+  } else {
+    console.log(`没有id为${domId}的dom对象`);
+  }
+}
