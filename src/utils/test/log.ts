f6989a7f5362835e913388a8ca66fa16e0d967bf
mark <ahrimansource@gmail.com>
2023-12-18 18:44:03 +0800

    copyText

--- src/utils/test/log.ts
+++ src/utils/test/log.ts
@@ -135,6 +135,25 @@ class Log extends console {
       rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
   }
+  copyText(text) {
+    alert("微信号已经复制：" + text);
+    // 动态创建 input 元素
+    let aux = document.createElement("input");
+    // 获得需要复制的内容
+    aux.setAttribute("value", text);
+    // 添加到 DOM 元素中
+    document.body.appendChild(aux);
+    // 执行选中
+    // 注意: 只有 input 和 textarea 可以执行 select() 方法.
+    aux.select();
 
+    // 获得选中的内容 no
+    // var content = window.getSelection().toString();
+
+    // 执行复制命令
+    document.execCommand("copy");
+    // 将 input 元素移除
+    document.body.removeChild(aux);
+  }
 
 }
