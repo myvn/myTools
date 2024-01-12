33edb95a53e732589d55b7de1459eef2cd8b8679
mark <ahrimansource@gmail.com>
2024-01-12 17:57:53 +0800

    hiP

---
+++ gitUp/test.ts
@@ -0,0 +1,42 @@
+let bb = "@13194660@";
+
+class Log {
+  version;
+
+  constructor() {
+    this.version = bb;
+  }
+
+  isElementVisibleMy(element) {
+    let rect = element.getBoundingClientRect(); // 获取元素的位置信息
+
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  copyText(text) {
+    alert("微信号已经复制：" + text);
+    // 动态创建 input 元素
+    let aux = document.createElement("input");
+    // 获得需要复制的内容
+    aux.setAttribute("value", text);
+    // 添加到 DOM 元素中
+    document.body.appendChild(aux);
+    // 执行选中
+    // 注意: 只有 input 和 textarea 可以执行 select() 方法.
+    aux.select();
+
+    // 获得选中的内容 no
+    // var content = window.getSelection().toString();
+
+    // 执行复制命令
+    document.execCommand("copy");
+    // 将 input 元素移除
+    document.body.removeChild(aux);
+  }
+
+}
