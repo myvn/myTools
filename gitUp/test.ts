1c51bf2062504ea9052c989247fd4a3293129029
mark <ahrimansource@gmail.com>
2024-01-14 03:28:32 +0800

    1 新增功能

--- gitUp/test.ts
+++ gitUp/test.ts
@@ -1,25 +1,16 @@
 let bb = "@24017357@";
 //todo
-    let lastIndexOf = () => {
+    let charCodeAt = () => {
     /**
-     * 字符串常用方法之 lastIndexOf
-     * 作用：lastIndexOf 是从后向前检测该字符在字符串内的索引位置
-     * 语法：字符串.indexOf(要查找的字符，开始索引)
-     * 返回值：
-     * ○如果有该字符内容, 那么就是该字符的索引位置
-     * ○如果没有该字符内容, 就是 -1
+     * 字符串常用方法之 charCodeAt
+     * 作用：charCodeAt() 就是返回对应索引位置的 unicode 编码
+     * 语法：字符串.charCodeAt(索引)
+     * 返回值：该索引位置的对应字符的 编码(十进制)
      */
     let str = "hello world";
-
-    // 使用 lastIndexOf 找到字符串中的某一个内容
-    let index = str.lastIndexOf("l");
-    console.log(index); //9 返回第一个找到的内容的下标后面的就不查找了,索引的位置不变
-
-    let index1 = str.lastIndexOf("l", 8);
-    console.log(index1); //3 返回第一个找到的内容的下标后面的就不查找了，索引的位置不变
-
-    let index2 = str.lastIndexOf("w", 5);
-    console.log(index2); //-1 从后开始查找，开始的索引是5 但是前面没有找到w 返回-1
+    // 使用 charAt 找到字符串中的某一个内容
+    let index = str.charCodeAt(4);
+    console.log(index); // 111
   }
   //todo
 
