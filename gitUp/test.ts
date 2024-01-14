5841d62542eb09936802979592c71f476c1b3493
mark <ahrimansource@gmail.com>
2024-01-14 14:28:33 +0800

    2 修复bug

--- gitUp/test.ts
+++ gitUp/test.ts
@@ -1,6 +1,6 @@
 let bb = "@24017357@";
 //todo
-    let toLowerCase = () => {
+    let toUpperCase = () => {
     /**
      * 字符串常用方法之 toLowerCase 和 toUpperCase
      * 作用：这两个方法分别是用来给字母格式的字符串转成 小写字母 和 大写字母 的
@@ -10,10 +10,9 @@ let bb = "@24017357@";
      */
     var str = "hello world";
 
-
-    // 使用 toLowerCase 转换成小写
-    var lower = str.toLowerCase();
-    console.log(lower); // hello world
+    // 使用 toUpperCase 转换成大写
+    var upper = str.toUpperCase();
+    console.log(upper); // HELLO WORLD
 
   }
   //todo
