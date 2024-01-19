f2b97c350b00de20d9fabe308efdb82050e63970
mark <ahrimansource@gmail.com>
2024-01-19 13:52:14 +0800

    1 新增功能

--- gitUp/test.ts
+++ gitUp/test.ts
@@ -1,16 +1,27 @@
 let bb = "@24017357a1@";
 //todo
-    let charCodeAt = () => {
+    let split = () => {
     /**
-     * 字符串常用方法之 charCodeAt
-     * 作用：charCodeAt() 就是返回对应索引位置的 unicode 编码
-     * 语法：字符串.charCodeAt(索引)
-     * 返回值：该索引位置的对应字符的 编码(十进制)
+     * 字符串常用方法之 split
+     * 作用：按照切割符号, 把字符串切割开, 放在一个数组里面.
+     * 语法：字符串.split('指定的切割符')
+     * ○切割符可以不传递，就会和整体当做一个字符串
+     * ○(''）空字符串会一位一位的切割
+     * ○(' ') 字符串中有空格 会按照原字符串中的空格切割
+     * 返回值：一个用指定切割符切割好的数组
      */
-    let str = "hello world";
-    // 使用 charAt 找到字符串中的某一个内容
-    let index = str.charCodeAt(4);
-    console.log(index); // 111
+    var str = "hello world";
+
+// 使用 split 切割成一个数组
+    // @ts-ignore
+    var res = str.split();
+    console.log(res); //['hello world']
+
+    var res1 = str.split("");
+    console.log(res1); //['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']
+
+    var res2 = str.split(" ");
+    console.log(res2); //['hello', 'world']
   }
   //todo
 
