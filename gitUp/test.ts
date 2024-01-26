d4d0440c6739376c359c2f37bd2d47cc750ecd2f
mark <ahrimansource@gmail.com>
2024-01-26 12:02:19 +0800

    2 修复bug

--- gitUp/test.ts
+++ gitUp/test.ts
@@ -1,17 +1,23 @@
 let bb = "@24017357a1@";
 //todo
-    let substring = () => {
+    let concat = () => {
     /**
-     * 字符串常用方法之 substring
-     * 作用：substring 是用来截取字符串使用的
-     * 语法： substring(从哪个索引开始，到哪个索引截止)，包含开始索引，不包含结束索引
-     * 返回值：返回截取到的内容
+     * 字符串常用方法之 concat
+     * 作用：字符串拼接也可以说是字符串合并
+     * 语法：字符串.concat(字符串)
+     * 返回值：拼接后的字符串
      */
-    var str = "hello world";
 
-    // 使用 substring截取字符串中的某一个内容
-    var res = str.substring(2, 8);
-    console.log(res); //llo wo
+    var str = "hello world ";
+
+    var str1 = "ni hao";
+
+    // 使用 concat 切割成一个数组
+    var res = str.concat("ni hao");
+    console.log(res); // hello world ni hao
+
+    var res1 = str.concat(str1);
+    console.log(res1); // hello world ni hao
   }
   //todo
 
