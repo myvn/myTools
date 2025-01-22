6f29fa36b158143123edbdf0d45f31e37ff7b80e
myvn <Ahrimansource@gmail.com>
2025-01-22 09:58:49 +0800

    1 新增功能

--- gitUp/test.ts
+++ gitUp/test.ts
@@ -1,19 +1,23 @@
 let bb = "@24017357a1@";
 //todo
-    let trim = () => {
+    let slice = () => {
     /**
-     * 字符串常用方法之 trim
-     *     作用：取出字符串头尾的空白内容
-     *     语法：字符串.trim()
-     *     返回值：去除空白内容以后的字符串
+     * 字符串常用方法之 slice
+     * 作用：截取字符串
+     * 语法：字符串.slice(起始索引，结束索引)
+     * ○包含开始的索引对应的内容，不包含结束索引对应的内容
+     * ○结束索引不写就直接截取到末尾
+     * 返回值：截取出来的字符串
      */
+    var str = "hello world";
 
+    // 使用 slice 截取字符串
+    var res = str.slice(1, 4); //ell
+    console.log(res);
+    //没有结束的索引直接截取到末尾
+    var res1 = str.slice(1); //ello world
+    console.log(res1);
 
-    var str = "   hello world     ";
-
-// 使用 trim 切割成一个数组
-    var res = str.trim();
-    console.log(res); // hello world
   }
   //todo
 
